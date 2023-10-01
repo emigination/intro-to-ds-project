b6dd1aa79f35cdbd3e9f94a135a2dc679edb3e87
Emilia Andersson <emilia.andersson@helsinki.fi>
2023-10-01 18:02:38 +0300

    remove weather imputations

--- saa.py
+++ saa.py
@@ -5,9 +5,8 @@ import datetime as dt
 year = 2021
 startMonth = 4
 endMonth = 10
-startDate = dt.datetime(year, startMonth, 1) - dt.timedelta(days=1)
-startTime = startDate.strftime('%Y-%m-%d') + "T00:00:00Z"
-endTime = (dt.datetime(year, endMonth+1, 1)).strftime('%Y-%m-%d')  + "T23:59:59Z"
+startTime = dt.datetime(year, startMonth, 1).strftime('%Y-%m-%d') + "T00:00:00Z"
+endTime = (dt.datetime(year, endMonth+1, 1) - dt.timedelta(days=1)).strftime('%Y-%m-%d')  + "T23:59:59Z"
 
 data = download_stored_query("fmi::observations::weather::daily::multipointcoverage", ["place=Helsinki", "starttime="+startTime, "endtime="+endTime]).data
 dateTimes = [dateTime for dateTime in list(data.keys()) if dateTime.strftime('%H') == '00']
@@ -18,43 +17,19 @@ for dateTime in dateTimes:
 
 temperatures = []
 precipitations = []
-for i in range(1, len(weatherList)-1):
-  temperature = weatherList[i]['Air temperature']['value']
-  if np.isnan(temperature):
-    previous = weatherList[i-1]['Air temperature']['value']
-    following = weatherList[i+1]['Air temperature']['value']
-    if np.isnan(previous):
-      temperatures.append(following)
-    elif np.isnan(following):
-      temperatures.append(previous)
-    else:
-      temperature = (previous + following)/2
-  else:
-    temperatures.append(temperature)
-
-  precipitation = weatherList[i]['Precipitation amount']['value']
+for daysWeather in weatherList:
+  temperatures.append(daysWeather['Air temperature']['value'])
+  precipitation = daysWeather['Precipitation amount']['value']
   if precipitation == -1:
-    precipitation = 0
+    precipitation = 0.0
   precipitations.append(precipitation)
 
 outfile = open('temperatures.txt', 'w')
 outfile.writelines([str(i)+'\n' for i in temperatures])
 outfile.close()
 
-imputedPrecipitations = precipitations.copy()
-for i, precipitation in enumerate(precipitations):
-  if np.isnan(precipitation):
-    previous = precipitations[i-1]
-    following = precipitations[i+1]
-    if np.isnan(previous):
-      imputedPrecipitations[i] = following
-    elif np.isnan(following):
-      imputedPrecipitations[i] = previous
-    else:
-      imputedPrecipitations[i] = (previous + following)/2
-
 outfile = open('precipitations.txt', 'w')
-outfile.writelines([str(i)+'\n' for i in imputedPrecipitations])
+outfile.writelines([str(i)+'\n' for i in precipitations])
 outfile.close()
 
 meanWindSpeeds = []
