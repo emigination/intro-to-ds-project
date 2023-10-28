4a04159028bd8ac5da31a1ff177382349ec8fad4
Emilia Andersson <emilia.andersson@helsinki.fi>
2023-10-28 23:21:37 +0300

    valmis kartta

--- kartta.py
+++ kartta.py
@@ -1,5 +1,6 @@
 import geopandas
 import matplotlib.pyplot as plt
+import geoplot as gplt
 import pandas as pd
 
 municipality_borders = geopandas.read_file('data/kuntarajat-uusimaa.geojson')
@@ -7,11 +8,10 @@ districts_with_sea = geopandas.read_file('data/Helsingin-piirijakorajat-eri-vuos
 helsinki_borders = municipality_borders[municipality_borders["name"] == "Helsinki"]
 districts = districts_with_sea.clip(helsinki_borders)
 
-districts.plot()
-
-plt.show()
-
 trip_data = pd.read_csv('data/district_trips lopullinen.csv')
-total_durations = trip_data.groupby('district')['Total ride duration / population'].sum()
+total_durations = trip_data.loc[trip_data.Year == 2021].groupby('district')['Total ride duration / population'].sum()
 
-df = pd.merge(total_durations, districts, left_on='district', right_on='NIMI_ISO')
+merged_data = pd.merge(districts, total_durations, left_on='NIMI_ISO', right_on='district').explode()
+
+gplt.choropleth(merged_data, hue='Total ride duration / population', cmap='Blues', edgecolor='white', linewidth=1,
+                legend=True, legend_kwargs={'location': 'bottom'}, projection=gplt.crs.WebMercator()).get_figure().savefig('map.svg', bbox_inches='tight')
