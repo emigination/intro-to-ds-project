a7bf7714485731516dcedad3c8ae34458a4baf92
Emilia Andersson <emilia.andersson@helsinki.fi>
2023-10-28 18:12:14 +0300

    ajokestot karttadataan

--- kartta.py
+++ kartta.py
@@ -1,5 +1,6 @@
 import geopandas
 import matplotlib.pyplot as plt
+import pandas as pd
 
 municipality_borders = geopandas.read_file('data/kuntarajat-uusimaa.geojson')
 districts_with_sea = geopandas.read_file('data/Helsingin-piirijakorajat-eri-vuosilta-gpkg/v2020.gpkg', layer='peruspiirit-2020').to_crs("EPSG:4326")
@@ -9,3 +10,8 @@ districts = districts_with_sea.clip(helsinki_borders)
 districts.plot()
 
 plt.show()
+
+trip_data = pd.read_csv('data/district_trips lopullinen.csv')
+total_durations = trip_data.groupby('district')['Total ride duration / population'].sum()
+
+df = pd.merge(total_durations, districts, left_on='district', right_on='NIMI_ISO')
