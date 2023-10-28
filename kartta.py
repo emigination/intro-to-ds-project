5ca6db150a432b3dfca84bbe8a5e3c6d23f5fe73
Emilia Andersson <emilia.andersson@helsinki.fi>
2023-10-28 17:01:22 +0300

    korjattu helsingin alueet

--- kartta.py
+++ kartta.py
@@ -2,11 +2,10 @@ import geopandas
 import matplotlib.pyplot as plt
 
 municipality_borders = geopandas.read_file('data/kuntarajat-uusimaa.geojson')
-# districts =geopandas.read_file('data/Helsingin-piirijakorajat-eri-vuosilta-gpkg/v2020.gpkg', layer='Helsingin piirijakorajat 2020')
-districts = geopandas.read_file('data/Helsingin-piirijakorajat-eri-vuosilta-gpkg/v2020.gpkg').to_crs("EPSG:4326")
+districts_with_sea = geopandas.read_file('data/Helsingin-piirijakorajat-eri-vuosilta-gpkg/v2020.gpkg', layer='peruspiirit-2020').to_crs("EPSG:4326")
+helsinki_borders = municipality_borders[municipality_borders["name"] == "Helsinki"]
+districts = districts_with_sea.clip(helsinki_borders)
 
-helsinki = municipality_borders[municipality_borders["name"] == "Helsinki"]
-
-geopandas.overlay(districts, helsinki, how='intersection').plot()
+districts.plot()
 
 plt.show()
